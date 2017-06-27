66ccbf2a17cae2db15bddfddc72d7a588a1c9072
FrencisAlreadyInUse <francisdclercq@gmail.com>
2017-06-27 17:08:42 +0200

    deprecated stylelint fixes (id & universal)

--- index.js
+++ index.js
@@ -18,11 +18,13 @@ module.exports = {
     "string-quotes": `single`,
     "declaration-no-important": true,
     "no-duplicate-selectors": true,
-    "selector-no-id": true,
+    // deprecated 8.0 [selector-no-id]
+    "selector-max-id": 0,
     "number-max-precision": 2,
     "shorthand-property-no-redundant-values": true,
     "selector-no-qualifying-type": [true, {ignore: `attribute`}],
-    "selector-no-universal": true,
+    // deprecated 8.0 [selector-no-universal]
+    "selector-max-universal": 0,
     "selector-no-vendor-prefix": true,
     "property-no-vendor-prefix": true,
     // deprecated 7.8.0
